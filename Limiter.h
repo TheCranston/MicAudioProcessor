4f76054d0347173c53d04d2890a1ab6a64d7a294
clamrock <chris.lamrock@gmail.com>
2022-12-26 10:21:15 -0500

    Settings menu - added files

---
+++ Limiter.h
@@ -0,0 +1,15 @@
+//
+// Image: Limiter
+// dimension: 183x85
+// Size: 30kb
+//
+
+#pragma once
+
+#include <tgx.h>
+
+// the image object
+extern const tgx::Image<tgx::RGB565> Limiter;
+
+// end of file Limiter.h
+
